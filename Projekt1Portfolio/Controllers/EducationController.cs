605f2c052ffef3b3503ace91215714dbd83438ac
Yasin Kereci <kereciyasin52@gmail.com>
2024-09-11 10:25:58 +0200

    AdminController erstellt

---
+++ Projekt1Portfolio/Controllers/EducationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projekt1Portfolio.Controllers
+{
+    public class EducationController : Controller
+    {
+        // GET: Education
+        public ActionResult EducationList()
+        {
+            return View();
+        }
+
+        // burasi yeni bir giris alanidir.
+		public ActionResult CreateEducation()
+		{
+			return View();
+		}
+	}
+}
