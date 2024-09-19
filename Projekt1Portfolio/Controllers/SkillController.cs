badfe4e3944fde3664f51110525c1e9af7d18762
Yasin Kereci <kereciyasin52@gmail.com>
2024-09-19 19:44:11 +0200

    skill controller

---
+++ Projekt1Portfolio/Controllers/SkillController.cs
@@ -0,0 +1,18 @@
+﻿using Projekt1Portfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projekt1Portfolio.Controllers
+{
+    public class SkillController : Controller
+    {
+        MyPortfolio5DbEntities context = new MyPortfolio5DbEntities();  
+		public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
