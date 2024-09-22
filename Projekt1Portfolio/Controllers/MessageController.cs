d7dd044b0925b9ab7165eef93710eaf82ca70425
Yasin Kereci <kereciyasin52@gmail.com>
2024-09-22 12:32:16 +0200

    messagecontroller erstellt

---
+++ Projekt1Portfolio/Controllers/MessageController.cs
@@ -0,0 +1,18 @@
+﻿using Projekt1Portfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projekt1Portfolio.Controllers
+{
+    public class MessageController : Controller
+    {
+		MyPortfolio5DbEntities database = new MyPortfolio5DbEntities();   
+		public ActionResult Inbox()
+        {
+            return View();
+        }
+    }
+}
