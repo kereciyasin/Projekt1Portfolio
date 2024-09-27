fda6bd9601a0228a7726b1e00680041295f128a6
Yasin Kereci <kereciyasin52@gmail.com>
2024-09-27 15:46:52 +0200

    schritt 6

--- Projekt1Portfolio/Controllers/StatisticsController.cs
+++ Projekt1Portfolio/Controllers/StatisticsController.cs
@@ -16,10 +16,10 @@ namespace Projekt1Portfolio.Controllers
             int messageCountIsReadyByTrue = context.Tbl_Message.Where(x => x.IsRead == true).Count();   
             int messageCountIsReadyByFalse = context.Tbl_Message.Where(x => x.IsRead == false).Count();
             int skillCount = context.Tbl_Skill.Count();
-
             var totalSkillValue = context.Tbl_Skill.Sum(x => x.Value);  
             var averageSkillValue = context.Tbl_Skill.Average(x => x.Value);    
-
+            var getEmailFromProfile = context.Tbl_Profile.Select(x => x.Email).FirstOrDefault();    
+            
 
             return View();
         }
