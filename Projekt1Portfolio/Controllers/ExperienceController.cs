4779cb51a93a62face2341b8670a0f83ae6bec60
Yasin Kereci <kereciyasin52@gmail.com>
2024-09-27 09:27:48 +0200

    experience added

---
+++ Projekt1Portfolio/Controllers/ExperienceController.cs
@@ -0,0 +1,21 @@
+﻿using Projekt1Portfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Projekt1Portfolio.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        MyPortfolio5DbEntities database = new MyPortfolio5DbEntities();
+
+        public ActionResult Index()
+        {
+            var values = database.Tbl_Experience.ToList();
+            return View(values);
+        }
+    }
+}
