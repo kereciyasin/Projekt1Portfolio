f7c69145f4f656162eeaf396a65eb43115a983b7
Yasin Kereci <kereciyasin52@gmail.com>
2024-10-03 10:20:30 +0200

    update

--- Projekt1Portfolio/Controllers/DefaultController.cs
+++ Projekt1Portfolio/Controllers/DefaultController.cs
@@ -32,6 +32,13 @@ namespace Projekt1Portfolio.Controllers
             ViewBag.about = context.Tbl_About.Select(x => x.Title).FirstOrDefault();
             ViewBag.details = context.Tbl_About.Select(x => x.Detail).FirstOrDefault();
             ViewBag.image = context.Tbl_About.Select(x => x.ImageUrl).FirstOrDefault();    
+
+            ViewBag.address = context.Tbl_Profile.Select(x => x.Address).FirstOrDefault();
+            ViewBag.email = context.Tbl_Profile.Select(x => x.Email).FirstOrDefault();
+            ViewBag.phone = context.Tbl_Profile.Select(x => x.PhoneNumber).FirstOrDefault();
+            ViewBag.github = context.Tbl_Profile.Select(x => x.Github).FirstOrDefault();
+            
+
             return PartialView();
         }
     }
