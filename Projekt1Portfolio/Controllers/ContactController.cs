f2f33b919b712f1e8ddc73436d8dc5dac957d300
Yasin Kereci <kereciyasin52@gmail.com>
2024-10-04 11:11:35 +0200

    added

---
+++ Projekt1Portfolio/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using Projekt1Portfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projekt1Portfolio.Controllers
+{
+    public class ContactController : Controller
+    {
+        MyPortfolio5DbEntities db = new MyPortfolio5DbEntities();   
+        public ActionResult PartialContactSideBar()
+        {
+            return PartialView();
+        }
+        public ActionResult PartialContactAddress()
+        {
+            ViewBag.Desciption = db.Tbl_Profile.Select(x => x.Description).FirstOrDefault();
+            ViewBag.Address = db.Tbl_Profile.Select(x => x.Address).FirstOrDefault();
+            ViewBag.Phone = db.Tbl_Profile.Select(x => x.PhoneNumber).FirstOrDefault();
+            ViewBag.Email = db.Tbl_Profile.Select(x => x.Email).FirstOrDefault();
+
+            return View();
+        }
+    }
+}
