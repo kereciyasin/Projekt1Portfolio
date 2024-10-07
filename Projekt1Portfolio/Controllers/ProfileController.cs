180dd527722208888d8ddc11e87ea6b76a4290c0
Yasin Kereci <kereciyasin52@gmail.com>
2024-10-07 11:11:38 +0200

    update

---
+++ Projekt1Portfolio/Controllers/ProfileController.cs
@@ -0,0 +1,26 @@
+﻿using Projekt1Portfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projekt1Portfolio.Controllers
+{
+    public class ProfileController : Controller
+    {
+        MyPortfolio5DbEntities db = new MyPortfolio5DbEntities();   
+        // GET: Profile
+        public ActionResult Index()
+
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var values = db.Tbl_Profile.ToList();   
+            return View(values);
+        }
+    }
+}
